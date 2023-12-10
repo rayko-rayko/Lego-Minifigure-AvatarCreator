4aee7733e0c41654362f621354921f8e953f4314
koray <gunsevenkoray@icloud.com>
2023-12-10 14:27:03 +0300

    Initial Commit

---
+++ Lego-Minifigure-AvatarCreator/Lego_Minifigure_AvatarCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lego_Minifigure_AvatarCreatorApp.swift
+//  Lego-Minifigure-AvatarCreator
+//
+//  Created by Koray Günseven on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lego_Minifigure_AvatarCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
